7db9fd365d35a2217c1661810b1f64a7266804e3
Caique Nascimento <caiquemigueldes@gmail.com>
2021-09-17 10:55:25 -0300

    alteracoes

--- server/index.js
+++ server/index.js
@@ -3,15 +3,18 @@ const express = require('express')
 const cors = require("cors")
 const app = express()
 const port = 4002
+const cookieParser = require("cookie-parser")
 
 const {authenticate} = require("./services/AuthService")
 
 
 app.use(express.json())
 app.use(cors({credentials: true, origin: "*"}))
+app.use(cookieParser())
 app.use("/login", require("./controllers/LoginController"))
 app.use("/user", authenticate, require("./controllers/UserController"))
 app.use("/courses", authenticate, require("./controllers/CoursesController"))
+app.use("/auth", authenticate, require("./controllers/AuthController"))
 app.use("/", (req, res) => {res.send("inicio")})
 
 
