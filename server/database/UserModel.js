21ff97a077f4ab31b74aeb7a898fc644bb363bac
caiquesjc <caiquemigueldes@gmail.com>
2021-11-20 19:44:48 -0300

    campo nickname no insert

--- server/database/UserModel.js
+++ server/database/UserModel.js
@@ -9,13 +9,14 @@ const registerUser = (body) => {
       use_telephone,
       use_password,
       use_is_admin,
+      use_nickname
     } = body;
     Connection.query(
-      `INSERT INTO "user" (use_name, use_age, use_email, use_telephone, use_password, use_is_admin) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
-      [use_name, use_age, use_email, use_telephone, use_password, use_is_admin],
+      `INSERT INTO "user" (use_name, use_age, use_email, use_telephone, use_password, use_is_admin, use_nickname) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+      [use_name, use_age, use_email, use_telephone, use_password, use_is_admin, use_nickname],
       (error, results) => {
         if (results) resolve(`User created: ${results.rows}`);
-        reject(error);
+          reject(error);
       }
     );
   });
@@ -34,15 +35,15 @@ const updateUser = (body) => {
   return new Promise(function (resolve, reject) {
     const {
       use_name,
-      use_age,
+      use_nickname,
       use_email,
       use_telephone,
-      use_is_admin,
+      use_password,
       use_id
     } = body;
     Connection.query(
-      'UPDATE "user" SET use_name=$1, use_age=$2, use_email=$3, use_telephone=$4, use_is_admin=$7 WHERE use_id=$6 RETURNING *',
-      [use_name, use_age, use_email, use_telephone, use_is_admin, use_id],
+      'UPDATE "user" SET use_name=$1, use_nickname=$2, use_email=$3, use_telephone=$4, use_password=$5 WHERE use_id=$6 RETURNING *',
+      [use_name, use_nickname, use_email, use_telephone, use_password, use_id],
       (error, results) => {
         if (results) resolve(`User updated: ${results.rows[0]}`);
         reject(error);
