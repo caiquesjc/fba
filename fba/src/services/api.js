6ba0ce4c1ade46abf103636060564397a373886b
caiquesjc <caiquemigueldes@gmail.com>
2021-12-10 09:27:38 -0300

    crp

--- fba/src/services/api.js
+++ fba/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios"; //axios para criar a api
 
 const api = axios.create({
- //baseURL: "http://192.168.100.6:4002",
-  baseURL: "https://api-fba.herokuapp.com"
+ baseURL: "http://192.168.100.6:4002",
+  //baseURL: "https://api-fba.herokuapp.com"
 });
 
 export default api;
--- server/database/UserModel.js
+++ server/database/UserModel.js
@@ -12,7 +12,7 @@ const registerUser = (body) => {
       use_nickname
     } = body;
     Connection.query(
-      `INSERT INTO "user" (use_name, use_age, use_email, use_telephone, use_password, use_is_admin, use_nickname) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+      `INSERT INTO "user" (use_name, use_age, use_email, use_telephone, use_password, use_is_admin, use_nickname) VALUES ($1, $2, $3, $4, PGP_SYM_ENCRYPT($5,'AES_KEY'), $6, $7) RETURNING *`,
       [use_name, use_age, use_email, use_telephone, use_password, use_is_admin, use_nickname],
       (error, results) => {
         if (results) resolve(`User created: ${results.rows}`);
@@ -43,7 +43,7 @@ const updateUser = (body) => {
       use_id
     } = body;
     Connection.query(
-      'UPDATE "user" SET use_name=$1, use_nickname=$2, use_email=$3, use_telephone=$4, use_password=$5, use_age=$6 WHERE use_id=$7 RETURNING *',
+      `UPDATE "user" SET use_name=$1, use_nickname=$2, use_email=$3, use_telephone=$4, use_password=PGP_SYM_ENCRYPT($5,'AES_KEY'), use_age=$6 WHERE use_id=$7 RETURNING *`,
       [use_name, use_nickname, use_email, use_telephone, use_password, use_age, use_id],
       (error, results) => {
         if (results) resolve(`User updated: ${results.rows[0]}`);
@@ -68,7 +68,7 @@ const deletUser = (id) => {
 const getUser = (id) => {
   return new Promise(function (resolve, reject) {
     Connection.query(
-      `SELECT use_id, use_name, use_age, use_email, use_telephone, use_photo, use_nickname FROM "user" WHERE use_id=${id}`,
+      `SELECT use_id, use_name, use_age, use_email, use_telephone, use_photo, use_nickname, use_is_admin FROM "user" WHERE use_id=${id}`,
       (error, results) => {
         if (results) resolve(results.rows[0]);
         reject({ sucess: false, error: error });
