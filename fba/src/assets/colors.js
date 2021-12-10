9f0a4c0b826fb3a491dc3bee323d95eb9da31777
caiquesjc <caiquemigueldes@gmail.com>
2021-12-10 09:35:52 -0300

    res api

--- fba/src/assets/colors.js
+++
@@ -1,8 +0,0 @@
-const fbaColors = {
-  DarkGray: "#aaa",
-  Nero: "#212121",
-  DeepSkyBlue: "#00A6FF",
-  Withe: "#ffffff"
-};
-
-module.exports = { fbaColors };
--- fba/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"; //axios para criar a api
-
-const api = axios.create({
- baseURL: "http://192.168.100.6:4002",
-  //baseURL: "https://api-fba.herokuapp.com"
-});
-
-export default api;
